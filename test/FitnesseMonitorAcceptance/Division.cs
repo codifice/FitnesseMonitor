3ace7c4b247b0ff33b75c228f10221aa27e9a8d9
Martin Jarvis <29016371+martinjarvis@users.noreply.github.com>
2018-08-04 18:52:38 +0100

    Added optional wait parameter to the failing tests

--- test/FitnesseMonitorAcceptance/Division.cs
+++ test/FitnesseMonitorAcceptance/Division.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Threading;
 
     public class Division
     {
@@ -11,6 +12,9 @@
 
         [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Reviewed. Suppression is OK here.")]
         public decimal Numerator = 0.0m;
+
+        [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Reviewed. Suppression is OK here.")]
+        public int Wait = 0;
 #pragma warning restore S1104 // Fields should not have public accessibility
 
         public decimal Quotient()
@@ -18,6 +22,10 @@
             if (this.Denominator == 0)
             {
                 throw new NotSupportedException($"{nameof(this.Denominator)} cannot be zero");
+            }
+            if (this.Wait > 0)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(this.Wait));
             }
 
             return this.Numerator / this.Denominator;
